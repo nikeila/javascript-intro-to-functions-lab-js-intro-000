dcc746a326237c09c9d2ad72e33f0d15bab938bd
peaceful-blog-2599 <teachmyself.alissa@gmail.com>
2020-05-01 15:09:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+  return string.toUpperCase()
+};
+function whisper(string){
+  return string.toLowerCase()
+};
+function logShout(string){
+  console.log(logShout.toUpperCase)
+};
+function logWhisper(string){
+  console.log(logWhisper.toLowerCase)
+};
+function sayHiToGrandma(string){
+  if (sayHiToGrandma === sayHiToGrandma.lowercase){
+    return ("I can't hear you!")
+  }
+  if (sayHiToGrandma === sayHiToGrandma.uppercase){
+    return ("YES INDEED!")
+  }
+  if (sayHiToGrandma === "I love you, Grandma"){
+    return ("I love you, too.")
+  }
+}
